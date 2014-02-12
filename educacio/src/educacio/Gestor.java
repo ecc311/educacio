f678f8dc6c356c7fcd17d1fb8af85358267938d2
ecc311 <greewolf_3@hotmail.com>
2014-02-12 17:56:54 +0100

    pujat Gestor per conectarse a la bd i modificarla

--- educacio/src/educacio/Gestor.java
+++ educacio/src/educacio/Gestor.java
@@ -1,16 +1,63 @@
 package educacio;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class Gestor {
+	
+	private static Connection conn;
+	private String sql;
+	/*
+	 * inicialització del servlet.
+	 * (non-Javadoc)
+	 * @see javax.servlet.GenericServlet#init()
+	 */
+	public void init(){
+		
+		Context initContext;
+		try {
+			initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			NomRecursTxt recurs = new NomRecursTxt();
+			recurs.setFitxer("C:/recursos.txt");
+			DataSource ds = (DataSource) envContext.lookup(recurs.GetRecurs("BDconector"));
+			conn = ds.getConnection();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	
 	void setAlumne(Persona alumne){
 		
+		init();
+		try {
+			Statement stmt = conn.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sql = "INSERT INTO alumne SET id='"+alumne.getIdPersona()+"',nom='"+alumne.getNomPersona()+"',edat='"+alumne.getEdatPersona()+"',grup='"+alumne.getGrupPersona()+"',horari='"+alumne.getHorariPersona()+"',password='"+alumne.getPasswordPersona()+"'";
+		
+		
+		//ResultSet rs = stmt.executeUpdate(sql);
 		
 		alumne.getIdPersona();
-		alumne.getNomPersona();
-		alumne.getEdatPersona();
+
+		
 		alumne.getGrupPersona();
-		alumne.getHorariPersona();
-		alumne.getPasswordPersona();
+	
+		
 		
 		
 		
